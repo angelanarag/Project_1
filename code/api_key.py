eaa324dedd09952cd2b415d2ca25bb95d6ed0a9f
angelanarag <119763672+ARNarag@users.noreply.github.com>
2023-02-09 20:38:24 -0500

    Updated api_key

---
+++ code/api_key.py
@@ -0,0 +1,4 @@
+# Original BLS Bureau of Labor Statistics API_Key using gmail
+#bls_api_key = "dbd0304fb10f47958e5ac5111ef2877d"
+# New BLS key using yahoo
+bls_api_key = "194226e0c207429b990a60371911ef51"
